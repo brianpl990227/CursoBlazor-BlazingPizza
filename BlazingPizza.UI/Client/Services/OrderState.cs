cfe83c82bb758b3a1c0d2f333916fcedfaef0fd0
Brian Pérez López <brianpl990227@gmail.com>
2022-03-27 00:53:07 +0100

    Validations

---
+++ BlazingPizza.UI/Client/Services/OrderState.cs
@@ -0,0 +1,47 @@
+﻿using BlazingPizza.UI.Shared;
+
+namespace BlazingPizza.UI.Client.Services
+{
+    public class OrderState
+    {
+        public Pizza ConfiguringPizza { get; private set; }
+        public bool ShowingConfigurePizza { get; private set; }
+        public Order order { get; private set; } = new Order();
+
+        public void ShowConfigurePizzaDialog(PizzaSpecial special)
+        {
+            ConfiguringPizza = new Pizza
+            {
+                Special = special,
+                SpecialId = special.Id,
+                Size = Pizza.DefaultSize,
+                Toppings = new List<PizzaTopping>()
+            };
+            ShowingConfigurePizza = true;
+        }
+
+        public void CancelConfigurePizzaDialog()
+        {
+            ConfiguringPizza = null;
+            ShowingConfigurePizza = false;
+        }
+
+        public void ConfirmConfigurePizzaDialog()
+        {
+            order.Pizzas.Add(ConfiguringPizza);
+            ConfiguringPizza = null;
+            ShowingConfigurePizza = false;
+        }
+
+        public void RemoveConfiguredPizza(Pizza pizza)
+        {
+            order.Pizzas.Remove(pizza);
+        }
+
+        public void ResetOrder()
+        {
+            order = new Order();
+        }
+
+    }
+}
--- BlazingPizza.UI/Shared/Address.cs
+++ BlazingPizza.UI/Shared/Address.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazingPizza.UI.Shared
 {
     public class Address
     {
+
         public int? Id { get; set; }
+        [Required(ErrorMessage = "¿Quién recibirá la orden?"), MaxLength(100)]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "Es necesario especificar dirección de envío"), MaxLength(100)]
         public string? Line1 { get; set; }
+        [MaxLength(100, ErrorMessage = "No se puede exceder a 100 caracteres")]
         public string? Line2 { get; set; }
+        [Required(ErrorMessage = "La ciudad debe ser especificada"), MaxLength(50)]
         public string? City { get; set; }
+        [Required(ErrorMessage = "El estado debe ser especificado"), MaxLength(20)]
         public string? Region { get; set; }
+        [Required(ErrorMessage = "El código postal debe ser especificado"), MaxLength(20)]
         public string? PostalCode { get; set; }
     }
 }
